a1bec65b49100861ace82a8004d8cb8f018e823a
Duckduck1596 <108049138+Duckduck1596@users.noreply.github.com>
2022-11-05 02:42:56 +0900

    List and Info upload

--- MarketApp/ProductList/ListView.swift
+++ MarketApp/ProductList/ListView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack{
+        HStack{ Image(systemName: "bell").resizable()
+                .frame(width: 25, height: 25, alignment: .trailing)
+               Spacer()
+            Text("App Name")
+                .font(.system(size: 33, weight: .bold))
+                .padding(.trailing, 45)
+            HStack{
+            Image(systemName: "magnifyingglass").resizable()
+                                   .frame(width: 25, height: 25, alignment: .trailing)
+            Image(systemName: "cart").resizable()
+                                .frame(width: 25, height: 25, alignment: .trailing)
+            }
+        }
+            Spacer()
+            
+            HStack{
+                frame(width: 200, height: 200, alignment: .center)
+                    .background(.gray)
+                
+            }
+        }
 }
-
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
         ListView()
     }
+}
 }
